49dfed3e047641e2d2827e0f281748e595370352
Sanjeevani Bhandari <sanjeevanibhandari@Sanjeevani-Bhandari-I156s-MacBook-Pro.local>
2022-04-07 11:46:04 +0530

    react router integrated , added 4 navbar links named as home , about , profule and login , login page part is remaining, it will show a form after you get logged into it and you can add details in your form theough that

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc imperdiet
+      nulla nec interdum egestas. Phasellus ligula purus, mollis eu velit ac,
+      efficitur dictum dolor. Mauris mollis eleifend libero, ac fringilla purus
+      volutpat eu. Praesent a elementum nisl. Etiam vehicula dignissim libero
+      sit amet blandit. Sed lacinia diam eu velit tincidunt, in tristique nisi
+      ullamcorper. Vestibulum non semper nibh. Sed vel dui ornare, fermentum
+      magna in, malesuada risus. Quisque ut nulla sit amet ligula posuere
+      vulputate at nec risus. Ut cursus augue non velit congue pharetra. Etiam
+      elit ante, viverra ut sagittis id, vulputate sit amet erat. 
+    </div>
+  );
+};
+export default About;
